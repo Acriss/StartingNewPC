a934228472606b6a7e65fc67467df3eac053d7a2
pierre <pme@activeviam.com>
2019-04-30 12:14:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starting a new Linux PC
+
+This script installs all that I need
---
+++ startup.sh
@@ -0,0 +1,4 @@
+## These tools are used for moving applications across displays and workspaces
+
+sudo apt-get install wmctrl
+sudo apt-get install xdotool
