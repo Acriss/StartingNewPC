35b85770967a8277a59bdc6d976e07e3be65e07e
pierre <pme@activeviam.com>
2019-04-30 13:19:53 +0200

    adding a few things

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
+## Assuming git is already downloaded
+sudo apt-get update
+
+sudo apt-get install curl
+sudo apt-get install tree
+
 ## These tools are used for moving applications across displays and workspaces
 
 sudo apt-get install wmctrl
 sudo apt-get install xdotool
+
+
+# Install Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
