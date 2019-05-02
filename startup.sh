3ac22d166d7fe6cce8c37e0497535352d2aeaa3c
pierre <pme@activeviam.com>
2019-05-02 13:50:46 +0200

    spotify and slack installs

--- startup.sh
+++ startup.sh
@@ -1,14 +1,32 @@
 ## Assuming git is already downloaded
 sudo apt-get update
 
-sudo apt-get install curl
-sudo apt-get install tree
+sudo apt-get install curl -y
+sudo apt-get install tree -y
 
 ## These tools are used for moving applications across displays and workspaces
 
 sudo apt-get install wmctrl
 sudo apt-get install xdotool
 
+# Install Spotify
+echo Installing Spotify
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo add-apt-repository ppa:dawidd0811/indicators
+
+sudo apt-get update
+sudo apt-get install spotify-client indicator-spotify
+
+# Install Slack
+sudo snap install slack --classic
+
 
 # Install Oh my zsh
+zsh --version || sudo apt install zsh && zsh --version 
+chsh -s $(which zsh)
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
